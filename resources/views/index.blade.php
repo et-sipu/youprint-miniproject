0295d016bd029c097d37b54be97394285c4fcb69
syahidnorazman <syahidnorazman@gmail.com>
2022-05-18 21:31:45 +0800

    Implement Dropify

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,22 +11,25 @@
 
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://jeremyfagis.github.io/dropify/dist/css/dropify.min.css">
 
         <!-- Datatable -->
         <link href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css" rel="stylesheet">
 
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
     <body style="background: #F2F2F2">
         <div class="container">
-            <div class="card card-default m-3">
-                <div class="row m-4">
-                    <div class="col-md-10"><div class="p-3" style="border: 1px dashed grey;border-radius: 5px;cursor:pointer"><p class="m-0 ml-3 text-left">Select file/Drag and drop</p></div></div>
-                    <div class="col-md-2"><button class="btn btn-default">Upload File</button></div>
+            <form action="">
+                <div class="card card-default m-3">
+                    <div class="row m-4">
+                        <div class="col-md-10"><input type="file" class="dropify" data-allowed-file-extensions="csv" required /></div>
+                        <div class="col-md-2"><button type="submit" class="btn btn-default">Upload File</button></div>
+                    </div>
                 </div>
-            </div>
+            </form>
             <div class="card card-default m-3 p-1">
                 <table id="datatable" class="table table-striped">
                     <thead>
@@ -53,11 +56,21 @@
         </div>
     </body>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://jeremyfagis.github.io/dropify/dist/js/dropify.min.js"></script>
     <script>
         $(document).ready( function () {
             $('#datatable').DataTable({
                 "paging": false,
                 "searching": false
+            });
+
+            $('.dropify').dropify({
+                messages: {
+                    'default': 'Select file/Drag and drop',
+                    'replace': 'Select file/Drag and drop to replace',
+                    'remove':  'Remove',
+                    'error':   'Ooops, something wrong happended.'
+                }
             });
         });
     </script>
